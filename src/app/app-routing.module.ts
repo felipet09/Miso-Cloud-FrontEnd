8f2ffcbd4b83603ff6f18664246befedcf5177ec
Felipe Triana <w.trianag@uniandes.edu.co>
2020-08-29 22:33:17 -0500

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterCompanyComponent } from './register-company/register-company.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingPageComponent},
+  {path:'registro_empresa',component:RegisterCompanyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
